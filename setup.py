7ddea2548eb8516c99f3f51f3cd6c4873863a096
swatidev14 <singhkendall14@gmail.com>
2025-03-23 19:48:56 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+name ='MLProject',
+version='0.0.1',
+author='Swati',
+author_email='singhkendall14@gmail.com',
+packages=find_packages(),
+install_requires =['pandas','numpy','seaborn']
+)
