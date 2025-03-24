c192dceb9f90abf95f8e3f3b66acc3104b07e02e
swatidev14 <singhkendall14@gmail.com>
2025-03-24 14:30:32 +0530

    new dataset

--- src/exception.py
+++ src/exception.py
@@ -1,10 +1,16 @@
 import sys
-import logging
+from logger import logging
 def error_message_detail(error, error_details:sys):
     _,_,exc_tb = error_details.exc_info()
-    file_name = exc_tb.tbframe.f_code.co_filename
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(file_name, exc_tb.tb_lineno, str(error))
-    return error_message                                                                                                        
+    if exc_tb is not None:
+        try:
+            file_name = exc_tb.tbframe.f_code.co_filename
+            error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(file_name, exc_tb.tb_lineno, str(error))
+            return error_message     
+        except AttributeError:
+            return f"Error: {str(error)}" # handle rare attribute error.
+    else:
+        return f"Error: {str(error)}" # handle case where exc_tb is None.                                                                                                   
                                                                                                           
 
 class CustomException(Exception):
@@ -16,3 +22,10 @@ class CustomException(Exception):
         return self.error_message
     
 
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        raise CustomException(e,sys)
+        logging.info("Divided by 0")
+        
